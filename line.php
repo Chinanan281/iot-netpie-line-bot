1352667cf9bc658575e2152e377ac48766d00834
Chinanan281 <68512895+Chinanan281@users.noreply.github.com>
2021-03-17 18:34:16 +0700

    Update line.php

--- line.php
+++ line.php
@@ -22,9 +22,9 @@ if ( sizeof($request_array['events']) > 0 )
    {
 		$text = $event['message']['text'];
 		
-	   	//if($text == "ชื่อ" || $text == "ชื่ออะไร" || $text == "ชื่ออะไรครับ"|| $text == "ชื่ออะไรคะ"){
-		//	$reply_message = 'ชื่อของฉันคือ ANGRYBOT';
-		//}
+	   	if($text == "ชื่อ" || $text == "ชื่ออะไร" || $text == "ชื่ออะไรครับ"|| $text == "ชื่ออะไรคะ"){
+			$reply_message = 'ชื่อของฉันคือ ANGRYBOT';
+		}
 	   
 	   	if($text == "สถานการณ์โควิดวันนี้" || $text == "covid19" || $text == "covid-19" || $text == "Covid-19"){
 		   $url = 'https://covid19.th-stat.com/api/open/today';
@@ -39,7 +39,7 @@ if ( sizeof($request_array['events']) > 0 )
 		   
 		   $obj = json_decode($result);
 		   
-		   //$reply_message = $result;
+		   $reply_message = $result;
 		   $reply_message = 'ติดเชื้อสะสมโว้ยยย!!! '. $obj->{'Confirmed'} .' คน รักษาหายแล้ว '.$obj->{'Recovered'} . ' คน';
 	
 	        }	
